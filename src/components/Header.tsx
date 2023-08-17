31e9616b818856d81f6bb3a9e7bb6317eb435b27
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2023-08-17 20:45:36 +0000

    add a login buton

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,26 +2,50 @@ import React from 'react'
 import styled from 'styled-components';
 
 export const Header = () => {
+
+  const onLoginPressed = () => {
+    console.log('login pressed');
+  }
+
   return (
     <HeaderContainer>
-      <h1>KgOpinion.com</h1>
+      <h2>KgOpinion.com</h2>
+      <AccountContainer onClick={onLoginPressed}>
+        Login
+      </AccountContainer>
     </HeaderContainer>
   )
 }
 
 const primaryColor = '#007bff';
-const secondarColor = '#0d6153';
+const secondarColor = '#32bda6';
 
 
 // styles for header
 const HeaderContainer = styled.header`
-  height: ${window.innerHeight * 0.1}px;
+  height: ${window.innerHeight * 0.08}px;
   width: 100%;
   background-color: ${primaryColor};
   color: #fff;
   display: flex;
   flex-direction: row;
   align-items: center;
-  justify-content: center;
+  justify-content: space-between;
+  border: 2px solid ${secondarColor};
   border-radius: 10px;
+`
+
+const AccountContainer = styled.button`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
+  border-radius: 10px;
+  margin-right: 10px;
+  padding: 5px;
+  background-color: #fff;
+  color: ${secondarColor};
+  border-color: ${secondarColor};
+
+
 `
